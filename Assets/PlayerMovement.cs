3b5f9d98ae7016754f1e71e432305367d05065fb
dariusmocan <dariusmocan2003@gmail.com>
2025-05-16 01:14:30 +0300

    gata

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 5f;
+    public float mouseSensitivity = 100f;
+    float xRotation = 0f;
+
+    public Transform playerCamera;
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+
+    void Update()
+    {
+        // Mișcare
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        Vector3 direction = transform.right * horizontal + transform.forward * vertical;
+        transform.position += direction * speed * Time.deltaTime;
+
+        // Rotire cu mouse
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        playerCamera.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        transform.Rotate(Vector3.up * mouseX);
+    }
+}
---
+++ Assets/QuestionTrigger.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+public class QuestionTrigger : MonoBehaviour
+{
+    public GameObject questionPanel;
+    public GameObject interactionHint;
+    private bool isPlayerNear = false;
+
+    void Update()
+    {
+        if (isPlayerNear)
+        {
+            interactionHint.SetActive(true);
+
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                questionPanel.SetActive(true);
+                interactionHint.SetActive(false);
+                gameObject.SetActive(false); // dezactivezi obiectul
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            isPlayerNear = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            isPlayerNear = false;
+            interactionHint.SetActive(false);
+        }
+    }
+}
