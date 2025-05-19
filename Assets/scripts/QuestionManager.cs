02f3a7f05f414e4047ce222f4da287c78c93f39e
Sebastian Marginean <sebastianmarginean@Sebastians-MacBook-Pro.local>
2025-05-19 21:45:28 +0300

    Destry wall

---
+++ Assets/scripts/QuestionManager.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+using System.Collections;
+
+public class QuestionManager : MonoBehaviour
+{
+    public TextMeshProUGUI questionText;
+    public Button answer1;
+    public Button answer2;
+    public Button answer3;
+    public TextMeshProUGUI verdictText;
+    private int correctAnswer = 0;
+
+    void Start()
+    {
+        questionText.text = "Care este capitala Franței?";
+        answer1.GetComponentInChildren<TextMeshProUGUI>().text = "Paris";
+        answer2.GetComponentInChildren<TextMeshProUGUI>().text = "Berlin";
+        answer3.GetComponentInChildren<TextMeshProUGUI>().text = "Madrid";
+
+        answer1.onClick.AddListener(CorrectAnswer);
+        answer2.onClick.AddListener(WrongAnswer);
+        answer3.onClick.AddListener(WrongAnswer);
+
+        verdictText.text = "";
+    }
+
+    void CorrectAnswer()
+    {
+        correctAnswer++;
+        StartCoroutine(ShowVerdictAndHide("Corect!", Color.green));
+    }
+
+    void WrongAnswer()
+    {
+        StartCoroutine(ShowVerdictAndHide("Greșit!", Color.red));
+    }
+    public int getCorrectAnswer()
+    {
+        return correctAnswer;
+    }
+
+    IEnumerator ShowVerdictAndHide(string message, Color color)
+    {
+        verdictText.text = message;
+        verdictText.color = color;
+
+        yield return new WaitForSeconds(2f);
+
+        verdictText.text = "";
+        questionText.gameObject.SetActive(false);
+        answer1.gameObject.SetActive(false);
+        answer2.gameObject.SetActive(false);
+        answer3.gameObject.SetActive(false);
+    }
+}
---
+++ Assets/scripts/WallScript.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallScript : MonoBehaviour
+{
+    public QuestionManager correctAnswer;
+    public int goal = 1;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Găsește instanța QuestionManager din scenă dacă nu este atribuită
+        if (correctAnswer == null)
+        {
+            correctAnswer = FindObjectOfType<QuestionManager>();
+            if (correctAnswer == null)
+            {
+                Debug.LogError("Nu s-a găsit QuestionManager în scenă!");
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Verifică dacă referința la QuestionManager există
+        if (correctAnswer == null)
+        {
+            Debug.LogWarning("QuestionManager reference is null! Trying to find it again...");
+            correctAnswer = FindObjectOfType<QuestionManager>();
+            return;
+        }
+        
+        // Afișează valoarea curentă a răspunsurilor corecte
+        if (Time.frameCount % 60 == 0) // Afișează o dată la ~1 secundă pentru a nu umple consola
+        {
+            Debug.Log($"Current correct answers: {correctAnswer.getCorrectAnswer()}, Goal: {goal}");
+        }
+        
+        // Verifică condiția pentru distrugerea peretelui
+        if (correctAnswer.getCorrectAnswer() >= goal)
+        {
+            Debug.Log("Condition met! Wall should be destroyed.");
+            Destroy(gameObject);
+            Debug.Log("Wall Destroyed");
+        }
+    }
+}
