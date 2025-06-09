863e1bcf66d6ad56ff26684715a8086530713bbc
dariusmocan <dariusmocan2003@gmail.com>
2025-06-09 23:25:46 +0300

    coliziune pereti

---
+++ Assets/scripts/QuestionData.cs
@@ -0,0 +1,15 @@
+﻿[System.Serializable]
+public class Question
+{
+    public string question;
+    public string[] options;
+    public string correct_answer;
+}
+
+[System.Serializable]
+public class Category
+{
+    public Question[] IstorieȘiGeografie;
+    public Question[] ȘtiințăȘiTehnologie;
+    public Question[] ArtăȘiCultură;
+}
